426695ce9b03880504d9d59f5f7f0b93a6a222d0
JULIO CESAR LEDESMA MARTINEZ <125784445+JulioCLedesma@users.noreply.github.com>
2024-05-01 18:16:18 -0600

    added database access

--- web/index.php
+++ web/index.php
@@ -48,4 +48,31 @@ $app->get('/coolbeans', function(Request $request, Response $response, LoggerInt
   return $response;
 });
 
+
+// Add Database connection to Container
+$container->set(PDO::class, function() {
+  $dburl = parse_url(getenv('DATABASE_URL') ?: throw new Exception('no DATABASE_URL'));
+  return new PDO(sprintf(
+    "pgsql:host=%s;port=%s;dbname=%s;user=%s;password=%s",
+    $dburl['host'],
+    $dburl['port'],
+    ltrim($dburl['path'], '/'), // URL path is the DB name, must remove leading slash
+    $dburl['user'],
+    $dburl['pass'],
+  ));
+});
+
+$app->get('/db', function(Request $request, Response $response, LoggerInterface $logger, Twig $twig, PDO $pdo) {
+  $st = $pdo->prepare('SELECT name FROM test_table');
+  $st->execute();
+  $names = array();
+  while($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $logger->debug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+  return $twig->render($response, 'database.twig', [
+    'names' => $names,
+  ]);
+});
+
 $app->run();
