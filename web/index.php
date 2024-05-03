fcdd9832e711742de2034cc4d1d3d420781e9205
JULIO CESAR LEDESMA MARTINEZ <125784445+JulioCLedesma@users.noreply.github.com>
2024-05-02 20:14:13 -0600

    Rutas

--- web/index.php
+++ web/index.php
@@ -66,61 +66,6 @@ $app->get('/db', function(Request $request, Response $response, LoggerInterface 
   });
 
   
-// Mostrar todos los registros
-$app->get('/list', function(Request $request, Response $response, LoggerInterface $logger, Twig $twig, PDO $pdo) {
-    $stmt = $pdo->query('SELECT * FROM your_table');
-    $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $twig->render($response, 'list.twig', ['items' => $items]);
-});
 
-// Mostrar un registro individual
-$app->get('/view/{id}', function(Request $request, Response $response, LoggerInterface $logger, Twig $twig, PDO $pdo, $args) {
-    $stmt = $pdo->prepare('SELECT * FROM your_table WHERE id = :id');
-    $stmt->execute(['id' => $args['id']]);
-    $item = $stmt->fetch(PDO::FETCH_ASSOC);
-    return $twig->render($response, 'view.twig', ['item' => $item]);
-});
-
-// Editar un registro
-$app->get('/edit/{id}', function(Request $request, Response $response, LoggerInterface $logger, Twig $twig, PDO $pdo, $args) {
-    $stmt = $pdo->prepare('SELECT * FROM your_table WHERE id = :id');
-    $stmt->execute(['id' => $args['id']]);
-    $item = $stmt->fetch(PDO::FETCH_ASSOC);
-    return $twig->render($response, 'edit.twig', ['item' => $item]);
-});
-
-$app->post('/edit/{id}', function(Request $request, Response $response, LoggerInterface $logger, PDO $pdo, $args) {
-    // Procesar la edición del registro
-});
-
-// Eliminar un registro
-$app->get('/delete/{id}', function(Request $request, Response $response, LoggerInterface $logger, Twig $twig, PDO $pdo, $args) {
-    $stmt = $pdo->prepare('SELECT * FROM your_table WHERE id = :id');
-    $stmt->execute(['id' => $args['id']]);
-    $item = $stmt->fetch(PDO::FETCH_ASSOC);
-    return $twig->render($response, 'delete.twig', ['item' => $item]);
-});
-
-$app->post('/delete/{id}', function(Request $request, Response $response, LoggerInterface $logger, PDO $pdo, $args) {
-    // Procesar la eliminación del registro
-});
-
-// Agregar un registro
-$app->get('/add', function(Request $request, Response $response, LoggerInterface $logger, Twig $twig) {
-    return $twig->render($response, 'add.twig');
-});
-
-$app->post('/add', function(Request $request, Response $response, LoggerInterface $logger, PDO $pdo) {
-
-    $name = $request->getParsedBody()['name']; // Suponiendo que el formulario tiene un campo llamado "name"
-
-
-    // Insertar el nuevo registro en la base de datos
-    $stmt = $pdo->prepare('INSERT INTO your_table (name) VALUES (:name)');
-    $stmt->execute(['name' => $name]);
-
-    // Redirigir a la página principal u otra página según sea necesario
-    return $response->withHeader('Location', '/');
-});
 
 $app->run();
